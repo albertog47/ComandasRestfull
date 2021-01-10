aa49a9690422916a1b0b309d570d31565b991875
Alberto García <albertog47@hotmail.com>
2021-01-10 18:08:02 +0100

    configuraciones y conexión heroku

--- src/main/java/com/comandas/restfull/ComandasRestfullApplication.java
+++ src/main/java/com/comandas/restfull/ComandasRestfullApplication.java
@@ -2,9 +2,16 @@ package com.comandas.restfull;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ComandasRestfullApplication {
+public class ComandasRestfullApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ComandasRestfullApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComandasRestfullApplication.class, args);
